e21a20102c16717fb078bb04415e62cec637b305
Osman Jalloh <omudu45@hotmail.com>
2016-08-19 23:00:37 +0000

    add routes for accessing the api.

--- server/urls.py
+++ server/urls.py
@@ -13,9 +13,12 @@ Including another URLconf
     1. Add an import:  from blog import urls as blog_urls
     2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
-from django.conf.urls import include, url
-from django.contrib import admin
+from rest_framework.routers import DefaultRouter
+from retail.views import ChainViewSet,StoreViewSet,EmployeeViewSet
 
-urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
-]
+router = DefaultRouter()
+router.register(prefix='chains', viewset=ChainViewSet)
+router.register(prefix='stores', viewset=StoreViewSet)
+router.register(prefix='employees', viewset=EmployeeViewSet)
+
+urlpatterns = router.urls
